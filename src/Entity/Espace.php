3ec4b26b6f9e251e9c1f683354da7290e7713dd3
Quentin Verdier <quent1.verdier@gmail.com>
2022-12-12 09:01:54 +0100

    first commit

---
+++ src/Entity/Espace.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\EspaceRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: EspaceRepository::class)]
+class Espace
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $nom = null;
+
+    #[ORM\Column]
+    private ?int $superficie = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE, nullable: true)]
+    private ?\DateTimeInterface $dateOuverture = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE, nullable: true)]
+    private ?\DateTimeInterface $dateFermeture = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getSuperficie(): ?int
+    {
+        return $this->superficie;
+    }
+
+    public function setSuperficie(int $superficie): self
+    {
+        $this->superficie = $superficie;
+
+        return $this;
+    }
+
+    public function getDateOuverture(): ?\DateTimeInterface
+    {
+        return $this->dateOuverture;
+    }
+
+    public function setDateOuverture(?\DateTimeInterface $dateOuverture): self
+    {
+        $this->dateOuverture = $dateOuverture;
+
+        return $this;
+    }
+
+    public function getDateFermeture(): ?\DateTimeInterface
+    {
+        return $this->dateFermeture;
+    }
+
+    public function setDateFermeture(?\DateTimeInterface $dateFermeture): self
+    {
+        $this->dateFermeture = $dateFermeture;
+
+        return $this;
+    }
+}
